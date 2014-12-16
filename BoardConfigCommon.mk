b64702d7de876a730664cc51b78defe1c254beb0
spezi77 <spezi7713@gmail.com>
2014-12-16 23:13:45 +0100

    Fix kernel compile error with duplicate toolchain path
    Change-Id: I015dec7ebd8f7d1aa0e3d1f5fa437c5d39a0f9f9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -64,7 +64,6 @@ TARGET_USES_ION := true
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/htc/qsd8k
 BUILD_KERNEL := true
-KERNEL_TOOLCHAIN_PREFIX:=$(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8
 TARGET_RAMDISK_COMPRESSION := xz --check=crc32 --arm --lzma2=dict=1MiB
 
 # Fix slow boot
