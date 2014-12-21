2aa59e757e87382bc637e919303f2f2082a0ce06
spezi77 <spezi7713@gmail.com>
2014-12-21 11:11:38 +0100

    Update media for L Big thanks to maniacx: https://github.com/maniacx/android_device_htc_leo/commit/7b110565a79198c195c6393fdefdba825f280270
    Change-Id: I05a96f7f39b0d00e45cb0122a6ca941e440633cc

--- qsd8k.mk
+++ qsd8k.mk
@@ -207,5 +207,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+
 # Proprietary blobs
 $(call inherit-product-if-exists, vendor/htc/qsd8k-common/qsd8k-vendor.mk)
