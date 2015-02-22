1d7598d542d0ca90d60b7c2254692ab1e3cf5a96
spezi77 <spezi7713@gmail.com>
2015-02-22 15:09:17 +0100

    Remove KSM setup from init qcom script (moved to init.bravo.rc)
    Change-Id: I118140f8611f0ead58d76b24c15ab11da7b7c044

--- init.qcom.post_boot.sh
+++ init.qcom.post_boot.sh
@@ -12,9 +12,5 @@ case "$target" in
         # mfreq to set CPU freq to max for performance
         chown root.system /sys/devices/system/cpu/mfreq
         chmod 220 /sys/devices/system/cpu/mfreq
-        # Configure and enable KSM
-        echo 1000 > /sys/kernel/mm/ksm/sleep_millisecs
-        echo 100 > /sys/kernel/mm/ksm/pages_to_scan
-        echo 1 > /sys/kernel/mm/ksm/run
     ;;
 esac
