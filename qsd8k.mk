a4c6d08bd6366ebf65987a7fd09c2447e1d12161
spezi77 <spezi7713@gmail.com>
2015-04-03 10:51:22 +0200

    Add and update tweaks by pixelfreak & walter79 * source: https://github.com/HD2-goes-future/android_tweaks
    Change-Id: I88403914084451c9a0fc09a7b016ec3aab1674e3

--- qsd8k.mk
+++ qsd8k.mk
@@ -81,11 +81,49 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.hwc.fakevsync=1 \
     debug.gr.numframebuffers=2 \
     debug.performance.tuning=1 \
-    debug.hwui.render_dirty_regions=false \
     persist.sys.ui.hw=1 \
     video.accelerate.hw=1 \
     view.scroll_friction=0 \
     ro.zygote.disable_gl_preload=true
+
+#
+# HWUI TWEAKS (credits pixelfreak & walter79)
+#
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.disable_scissor_opt=false \
+    ro.hwui.texture_cache_size=24 \
+    ro.hwui.layer_cache_size=16 \
+    ro.hwui.gradient_cache_size=0.5 \
+    ro.hwui.patch_cache_size=128 \
+    ro.hwui.path_cache_size=4 \
+    ro.hwui.shape_cache_size=1 \
+    ro.hwui.drop_shadow_cache_size=2 \
+    ro.hwui.r_buffer_cache_size=2 \
+    ro.hwui.texture_cache_flush_rate=0.6 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=256 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_large_cache_height=512 \
+    hwui.text_gamma_correction=lookup \
+    hwui.text_gamma=1.4 \
+    hwui.text_gamma.black_threshold=64 \
+    hwui.text_gamma.white_threshold=192 \
+    hwui.use_gpu_pixel_buffers=true \
+    hwui.render_dirty_regions=false \
+    hwui.use.blacklist=true
+
+# WEBVIEW / is only for 2.x kernel and this fix blackscreen in google apps
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.webview.provider=classic
+
+# DISABLE ERROR CHECKING
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.kernel.android.checkjni=0
+
+# MULTI-CORE DEX OPT DISABLED
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.dalvik.multithread=false
 
 #
 # Low Mem tweaks
@@ -114,15 +152,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Telephony/ring Tweaks
 #
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.call_ring.delay=0 \
-    ring.delay=0
+    ro.telephony.call_ring.multiple=false \
+    ro.telephony.call_ring.delay=3000
 
 #
 # Proximity (Disable blackscreen issue after call)
 #
 PRODUCT_PROPERTY_OVERRIDES += \
-    mot.proximity.delay=0 \
-    ro.lge.proximity.delay=25
+    mot.proximity.delay=25 \
+    ro.lge.proximity.delay=25 \
+    ring.delay=0
 
 #
 # Scrolling tweaks
@@ -150,6 +189,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sys.fw.bg_apps_limit=4 \
     sys.mem.max_hidden_apps=4 \
     ro.config.max_starting_bg=2
+
+# MAKES APPS LOAD FASTER AND FREES MORE RAM
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dexopt-flags=v=a,o=v,m=y,u=y
 
 # Default heap settings for 512mb device
 include frameworks/native/build/phone-hdpi-512-dalvik-heap.mk
