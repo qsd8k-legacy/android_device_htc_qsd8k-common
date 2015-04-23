7abbe30e7758175bc27f4421b7bd43fdc5a900c2
spezi77 <spezi7713@gmail.com>
2015-04-23 21:52:41 +0200

    Fix compile error caused by hwui tweak * error: ro.hwui.texture_cache_flush_rate cannot exceed 31 bytes: ro.hwui.texture_cache_flush_rate (32)

--- qsd8k.mk
+++ qsd8k.mk
@@ -96,7 +96,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.hwui.shape_cache_size=1 \
     ro.hwui.drop_shadow_cache_size=2 \
     ro.hwui.r_buffer_cache_size=2 \
-    ro.hwui.texture_cache_flush_rate=0.6 \
     ro.hwui.text_small_cache_width=1024 \
     ro.hwui.text_small_cache_height=256 \
     ro.hwui.text_large_cache_width=2048 \
