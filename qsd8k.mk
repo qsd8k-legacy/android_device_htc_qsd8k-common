d3f9c9ffdb91374205986ee4ab0ef70156d25e46
Michael Bestas <mikeioannina@gmail.com>
2015-05-10 02:02:36 +0300

    Disable dex2oat watchdog
    QSD8K is too slow to compile some very big APKs within the 6 mins limit
    so we get the "Optimizing apps" dialog on each boot and dex2oat never
    completes properly, slowing down the whole system
    
    Change-Id: I01bc26287397cacd4175e204c33b8da63a8bfb8a

--- qsd8k.mk
+++ qsd8k.mk
@@ -201,6 +201,7 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 # as described here: http://source.android.com/devices/tech/dalvik/configure.html#with_art_small_mode
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-filter=interpret-only \
+    dalvik.vm.dex2oat-flags=--no-watch-dog \
     dalvik.vm.image-dex2oat-filter=speed
 
 # Allow bypassing setupwizard
