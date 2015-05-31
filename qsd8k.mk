e4aabbfb29cc704278d55b981ec350999fa35945
Michael Bestas <mikeioannina@gmail.com>
2015-05-31 17:46:31 +0300

    Set dalvik.vm.dex2oat-filter=balanced
    * ~11x runtime performance improvement
    
    Change-Id: I745634b66269bf5fbe51903cd0e2d8171e7dd333

--- qsd8k.mk
+++ qsd8k.mk
@@ -197,7 +197,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Use ART small mode
 # as described here: http://source.android.com/devices/tech/dalvik/configure.html#with_art_small_mode
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.dex2oat-filter=interpret-only \
+    dalvik.vm.dex2oat-filter=balanced \
     dalvik.vm.dex2oat-flags=--no-watch-dog \
     dalvik.vm.image-dex2oat-filter=speed
 
